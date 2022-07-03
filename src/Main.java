1a08709b6b040a6158a1c1bb19aea513a2d98a91
Julia <julia.panfilova@mail.ru>
2022-07-03 17:47:51 +0300

    Bonus Calculation added

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
+        int startAmount = 100;
+        int paymentAmount = 978;
+        int bonus;
+        int totalBalance = startAmount + paymentAmount;
+        boolean b = totalBalance > 1000;
+        int x = totalBalance/100;
+        if (b) {
+            bonus = x;
+        } else {
+            bonus = 0;
+        }
+        int s = totalBalance + bonus;
+
+        System.out.println("Вам начислено " + bonus + " бонусов");
+        System.out.println("Ваш итоговый баланс составляет " + s + " рублей");
+
+
+        // Условным оператором проверяете превысила ли
+        // сумма пополнения порог и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+    }
+}
