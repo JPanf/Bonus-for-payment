04ceb6a4abc997ce6789797d6691f85dc6b2b40a
Julia <julia.panfilova@mail.ru>
2022-07-04 23:26:09 +0300

    variable name changed

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@ public class Main {
         // Объявляете переменные для входных данных и
         // параметров программы: начального счёта,
         // суммы пополнения и тп
-        int startAmount = 100;
+        int startAmount = -100;
         int paymentAmount = 978;
         int bonus;
         int totalBalance = startAmount + paymentAmount;
         boolean b = totalBalance > 1000;
-        int x = totalBalance/100;
+        int bonusRuble = totalBalance/100;
         if (b) {
-            bonus = x;
+            bonus = bonusRuble;
         } else {
             bonus = 0;
         }
